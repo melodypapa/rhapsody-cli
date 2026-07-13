54cf3d6d5f5ee162267ca6c4b54905ff25057b2c
Melodypapa <melodypapa@outlook.com>
2026-07-13 19:59:15 +0800

    feat(variables): implement RPVariable and RPTag methods + register_wrapper

--- tests/unit/models/elements/test_variable.py
+++ tests/unit/models/elements/test_variable.py
@@ -112,3 +112,220 @@ def test_variable_set_type_declaration_delegates_to_com() -> None:
     variable.set_type_declaration("int*")
 
     fake.setTypeDeclaration.assert_called_once_with("int*")
+
+
+# =============================================================================
+# RPAttribute tests
+# =============================================================================
+
+
+def test_attribute_get_is_constant_returns_int() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsConstant=1)
+    attr = RPAttribute(fake)
+    assert attr.get_is_constant() == 1
+    fake.getIsConstant.assert_called_once_with()
+
+
+def test_attribute_get_is_constant_returns_zero() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsConstant=0)
+    attr = RPAttribute(fake)
+    assert attr.get_is_constant() == 0
+    fake.getIsConstant.assert_called_once_with()
+
+
+def test_attribute_get_is_ordered_returns_int() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsOrdered=1)
+    attr = RPAttribute(fake)
+    assert attr.get_is_ordered() == 1
+    fake.getIsOrdered.assert_called_once_with()
+
+
+def test_attribute_get_is_ordered_returns_zero() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsOrdered=0)
+    attr = RPAttribute(fake)
+    assert attr.get_is_ordered() == 0
+    fake.getIsOrdered.assert_called_once_with()
+
+
+def test_attribute_get_is_reference_returns_int() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsReference=1)
+    attr = RPAttribute(fake)
+    assert attr.get_is_reference() == 1
+    fake.getIsReference.assert_called_once_with()
+
+
+def test_attribute_get_is_reference_returns_zero() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute", getIsReference=0)
+    attr = RPAttribute(fake)
+    assert attr.get_is_reference() == 0
+    fake.getIsReference.assert_called_once_with()
+
+
+def test_attribute_set_is_constant_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_constant(True)
+
+    fake.setIsConstant.assert_called_once_with(1)
+
+
+def test_attribute_set_is_constant_false_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_constant(False)
+
+    fake.setIsConstant.assert_called_once_with(0)
+
+
+def test_attribute_set_is_ordered_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_ordered(True)
+
+    fake.setIsOrdered.assert_called_once_with(1)
+
+
+def test_attribute_set_is_ordered_false_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_ordered(False)
+
+    fake.setIsOrdered.assert_called_once_with(0)
+
+
+def test_attribute_set_is_reference_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_reference(True)
+
+    fake.setIsReference.assert_called_once_with(1)
+
+
+def test_attribute_set_is_reference_false_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPAttribute
+
+    fake = make_fake_element("Attribute")
+    attr = RPAttribute(fake)
+
+    attr.set_is_reference(False)
+
+    fake.setIsReference.assert_called_once_with(0)
+
+
+# =============================================================================
+# RPTag tests
+# =============================================================================
+
+
+def test_tag_get_base_wraps_result() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    base_com = make_fake_element("Class", getName="BaseClass")
+    fake = make_fake_element("Tag")
+    fake.getBase.return_value = base_com
+    tag = RPTag(fake)
+
+    result = tag.get_base()
+
+    fake.getBase.assert_called_once_with()
+    assert result.get_name() == "BaseClass"
+
+
+def test_tag_get_from_profile_returns_str() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag", getFromProfile="MyProfile")
+    tag = RPTag(fake)
+
+    assert tag.get_from_profile() == "MyProfile"
+    fake.getFromProfile.assert_called_once_with()
+
+
+def test_tag_get_multiplicity_returns_str() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag", getMultiplicity="0..*")
+    tag = RPTag(fake)
+
+    assert tag.get_multiplicity() == "0..*"
+    fake.getMultiplicity.assert_called_once_with()
+
+
+def test_tag_get_tag_meta_class_returns_str() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag", getTagMetaClass="Stereotype")
+    tag = RPTag(fake)
+
+    assert tag.get_tag_meta_class() == "Stereotype"
+    fake.getTagMetaClass.assert_called_once_with()
+
+
+def test_tag_get_value_returns_str() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag", getValue="some_value")
+    tag = RPTag(fake)
+
+    assert tag.get_value() == "some_value"
+    fake.getValue.assert_called_once_with()
+
+
+def test_tag_set_multiplicity_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag")
+    tag = RPTag(fake)
+
+    tag.set_multiplicity("1..*")
+
+    fake.setMultiplicity.assert_called_once_with("1..*")
+
+
+def test_tag_set_tag_meta_class_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag")
+    tag = RPTag(fake)
+
+    tag.set_tag_meta_class("NewStereotype")
+
+    fake.setTagMetaClass.assert_called_once_with("NewStereotype")
+
+
+def test_tag_set_value_delegates_to_com() -> None:
+    from rhapsody_cli.models.elements.variables.model_variables import RPTag
+
+    fake = make_fake_element("Tag")
+    tag = RPTag(fake)
+
+    tag.set_value("new_value")
+
+    fake.setValue.assert_called_once_with("new_value")
