56e290b8ae49ef958906af83ffb02d4d204bcfe3
melodypapa <melodypapa@outlook.com>
2026-08-02 15:02:19 +0800

    test: drop RPClassifier-duplicate and setup-only tests from RPClass suite

--- tests/integration/models/elements/classifiers/test_model_class.py
+++ tests/integration/models/elements/classifiers/test_model_class.py
@@ -8,7 +8,7 @@ from typing import cast
 
 import pytest
 
-from rhapsody_cli.models.elements.classifiers import RPClass, RPOperation
+from rhapsody_cli.models.elements.classifiers import RPOperation
 from rhapsody_cli.models.elements.containment import RPPackage, RPProject
 from rhapsody_cli.models.elements.interactions import RPEventReception
 
@@ -27,49 +27,6 @@ class TestRPClassIntegration:
         assert pkg is not None
         assert isinstance(pkg, RPPackage)
         return pkg
-
-    def test_create_class_in_package(self, test_project: RPProject) -> None:
-        pkg_name = self._unique("Pkg")
-        class_name = self._unique("TestClass")
-        pkg = self._create_package(test_project, pkg_name)
-        test_class = pkg.add_class(class_name)
-        assert test_class is not None
-        assert isinstance(test_class, RPClass)
-        assert test_class.get_name() == class_name
-        assert test_class.get_meta_class() == "Class"
-
-    def test_class_hierarchy_navigation(self, test_project: RPProject) -> None:
-        pkg_name = self._unique("NavPkg")
-        class_name = self._unique("ChildClass")
-        pkg = self._create_package(test_project, pkg_name)
-        test_class = pkg.add_class(class_name)
-        parent = test_class.get_owner()
-        assert parent is not None
-        assert parent.get_name() == pkg_name
-        assert isinstance(parent, RPPackage)
-
-    def test_create_operation_in_class(self, test_project: RPProject) -> None:
-        pkg_name = self._unique("OpPkg")
-        class_name = self._unique("OpClass")
-        pkg_name_op = f"{class_name}_op"
-        pkg = self._create_package(test_project, pkg_name)
-        test_class = pkg.add_class(class_name)
-        operation = test_class.add_operation(pkg_name_op)
-        assert operation is not None
-        assert isinstance(operation, RPOperation)
-        assert operation.get_name() == pkg_name_op
-        operations = test_class.get_operations()
-        assert operation in list(operations)
-
-    def test_class_delete(self, test_project: RPProject) -> None:
-        pkg_name = self._unique("DelPkg")
-        class_name = self._unique("DelClass")
-        pkg = self._create_package(test_project, pkg_name)
-        test_class = pkg.add_class(class_name)
-        test_class.delete_from_project()
-        classes = pkg.get_classes()
-        class_names = [c.get_name() for c in classes]
-        assert class_name not in class_names
 
     def test_class_inheritance(self, test_project: RPProject) -> None:
         pkg_name = self._unique("InhPkg")
